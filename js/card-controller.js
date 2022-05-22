1e1ef3e22b62eff7256970f22c2b422a07cbd2ce
ItamarAlves <itamaralvesprofi@gmail.com>
2022-05-21 22:02:09 -0400

    removendo card-model.js

--- js/card-controller.js
+++ js/card-controller.js
@@ -1,8 +1,45 @@
+const storage = window.localStorage;
+
 const cards = [
   {
     id: "1",
     holderName: "Itamarati A. Ribeirinho",
     validate: "06/22",
     number: ["1234", "4321", "1365", "8632"],
+    
+  },
+  {
+    id: "2",
+    holderName: "Itamarati A. Ribeirinho",
+    validate: "06/22",
+    number: ["1234", "4321", "1365", "8632"],
+  },
+  {
+    id: "3",
+    holderName: "Itamarati A. Ribeirinho",
+    validate: "06/22",
+    number: ["1234", "4321", "1365", "8632"],
+  },
+  {
+    id: "4",
+    holderName: "Itamarati A. Ribeirinho",
+    validate: "06/22",
+    number: ["1234", "4321", "1365", "8632"],
+  },
+  {
+    id: "5",
+    holderName: "Itamarati A. Ribeirinho",
+    validate: "06/22",
+    number: ["1234", "4321", "1365", "8632"],
+  },
+  {
+    id: "6",
+    holderName: "Itamarati A. Ribeirinho",
+    validate: "06/22",
+    number: ["1234", "4321", "1365", "8632"],
   },
 ];
+
+function addSaldo(descricao) {
+  
+}
--- js/card-model.js
+++
@@ -1,3 +0,0 @@
-const card = {
-  id: 6,
-};
